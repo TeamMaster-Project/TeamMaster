646b605e1fb77513d66d4c5dec1eb21e5f213861
mihinduranasinghe <mihindu.21523@gmail.com>
2020-05-04 23:39:37 +0530

    Create React App task_master

--- TM-backend-api-node/index.js
+++ TM-backend-api-node/index.js
@@ -8,9 +8,9 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
-require("./startup/prod")(app);
 
-const port = process.env.PORT || 3800;
+
+const port = process.env.PORT || 3700;
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
