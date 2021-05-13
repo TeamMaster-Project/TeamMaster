ed6da352fcc8e530be9b933c656964600ee83c07
Mihindu Ranasinghe <mihindu.21523@gmail.com>
2021-05-14 02:40:45 +0530

    UI Improvements

--- TM-frontend-react/team_master/src/components/Chatbox/index.jsx
+++ TM-frontend-react/team_master/src/components/Chatbox/index.jsx
@@ -7,30 +7,41 @@ import NewChatForm from './NewChatForm';
 import ChatSettingsTop from './ChatSettingsTop';
 import PreLoader from '../PreLoader/PreLoader';
 
-const IndexApp = () => {
-	// if (!localStorage.getItem('username')) return <LoginForm />;
-	let isLoading = true;
-	const currentUser = auth.getCurrentUser();
-	isLoading = false;
+class IndexApp extends Component {
+	state = {
+              isLoading: false,
+			  currentUser: {}
+      };
 
-	if(isLoading)
-		return <PreLoader/>
+	  async componentDidMount() {
+		  this.setState({isLoading: true});
+		  const currentUser = auth.getCurrentUser();
+		  this.setState({currentUser: currentUser})
+        setTimeout(()=>{
+            this.setState({isLoading: false});
+        },5000)
+    };
+	
+	render () {
+		if(this.state.isLoading)
+			return <PreLoader/>
 
-	return (
-		<div>
-			<ChatEngine
-				height='94vh'
-				projectID='5d514e59-2de9-4fa3-a915-764ea74ad722'
-				userName= {currentUser.email}
-				userSecret= {currentUser._id}
-				renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-				renderNewChatForm={(creds) => <NewChatForm {...creds}/>}
-				renderChatSettingsTop={(creds, chat) => <ChatSettingsTop creds={creds} chat={chat}/>}
-				renderPhotosSettings={(chat) => {}}
-				renderOptionsSettings={(creds, chat) => {}}
-			/>
-		</div>
-	);
+		return (
+			<div>
+				<ChatEngine
+					height='94vh'
+					projectID='5d514e59-2de9-4fa3-a915-764ea74ad722'
+					userName= {this.state.currentUser.email}
+					userSecret= {this.state.currentUser._id}
+					renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+					renderNewChatForm={(creds) => <NewChatForm {...creds}/>}
+					renderChatSettingsTop={(creds, chat) => <ChatSettingsTop creds={creds} chat={chat}/>}
+					renderPhotosSettings={(chat) => {}}
+					renderOptionsSettings={(creds, chat) => {}}
+				/>
+			</div>
+		);
+	}
 };
 
 export default IndexApp;
